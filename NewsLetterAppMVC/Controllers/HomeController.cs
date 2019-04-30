1626342df7cbfe6b111fd0bb05481955da5930a8
Michael G <mgalas2015@gmail.com>
2019-04-29 21:28:02 -0700

    Created connectionString to establish connection with our SQL Server database

--- NewsLetterAppMVC/Controllers/HomeController.cs
+++ NewsLetterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,6 +23,30 @@ namespace NewsLetterAppMVC.Controllers
             }
             else
             {
+                //Creating a connection string here to establish a connection with our database
+                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                //Creating a query here to insert data into the table we created in our newsletter database
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+
+                }
                 return View("Success");
             }
         }
@@ -39,3 +65,6 @@ namespace NewsLetterAppMVC.Controllers
         }
     }
 }
+
+
+ 
